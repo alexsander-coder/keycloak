375f25917ea0d3ae00e2c1b0a28ab3b2f8a5ed1b
Alexsander Hugo Gonçalves de Souza <asouza@valeshop.com.br>
2023-10-20 17:31:21 -0300

    redirectauth

---
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/login', (req, res) => {
+
+    const loginParams = new URLSearchParams({
+        client_id: 'fullcycle-client',
+        redirect_uri: 'http://localhost:3000/callback',
+        response_type: 'code',
+        scope: 'openid'
+    });
+
+    const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`
+    console.log(url);
+    res.redirect(url);
+});
+
+app.listen(3000, () => {
+    console.log('Listering on port 3000');
+})
